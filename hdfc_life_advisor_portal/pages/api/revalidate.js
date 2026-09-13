cfc72c27559ae361370126715ca409e6515549de
ParthGupta1004 <parthgupta5797@gmail.com>
2026-09-13 20:47:41 +0530

    feat: revalidation part is done

---
+++ hdfc_life_advisor_portal/pages/api/revalidate.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res)
+{
+    const {secret, path} = req.query
+
+    if(secret != process.env.REVALIDATE_SECRET)
+    {
+        return res.status(401).json({
+            ok: false
+        })
+    }
+
+    try
+    {
+        await res.revalidate(path)
+        return res.status(200).json({
+            ok: true,
+            path
+        })
+    }
+    catch(error)
+    {
+        console.error("Revalidation error:", error);
+        return res.status(500).json({
+            ok: false,
+            error: error.message,
+        })
+    }
+}
